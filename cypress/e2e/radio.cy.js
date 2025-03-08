c59a239d564d1c279ccff7eb01695ee5ce932f87
hebertmfrancato <hebertmfrancato@gmail.com>
2025-03-08 00:31:08 -0300

    Adiciona teste de radio button

---
+++ cypress/e2e/radio.cy.js
@@ -0,0 +1,14 @@
+describe('Radio Buttons', () => {
+
+    beforeEach(() => {
+        cy.goHome()
+        cy.login()
+        cy.userLoggedIn() // Checkpoint (Valida que o login foi bem-sucedido)
+        cy.goTo('/radio', 'Radio Buttons')
+    })
+
+    it('Deve mrarcar o framework usado no curos de Cypress Skills', () => {
+        cy.contains('label', 'Cypress')
+            .click()
+    });
+});
