badd0caba61ef8855d25336f12a6c08fd68fa3b8
hebertmfrancato <hebertmfrancato@gmail.com>
2025-03-09 00:49:37 -0300

    Adiciona teste de date picker

---
+++ cypress/e2e/datePicker.cy.js
@@ -0,0 +1,23 @@
+describe('Date Picker', () => {
+    
+    beforeEach(() => {
+        cy.goHome()
+        cy.login()
+        cy.userLoggedIn() // Checkpoint (Valida que o login foi bem-sucedido)
+        cy.goTo('/date-picker', 'Date Picker')
+    })
+
+    it('Selecionar uma data de aniversário', () => {
+        cy.get('input[placeholder="Escolha uma data"][readonly]') // readonly="readonly"
+            .click()
+
+        cy.get('select[aria-label="Month"]')
+            .select('Fevereiro')
+
+        cy.get('input[aria-label="Year"]')
+            .type('1992')
+
+        cy.get('span[aria-label="Fevereiro 18, 1992"]')
+            .click()
+    })
+});
