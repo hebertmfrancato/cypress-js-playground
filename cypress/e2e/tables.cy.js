fa269f8a41cfa59bf8bdcd80772fa8330bf0810a
hebertmfrancato <hebertmfrancato@gmail.com>
2025-03-09 11:40:16 -0300

    Adiciona teste de tabela

---
+++ cypress/e2e/tables.cy.js
@@ -0,0 +1,39 @@
+describe('Tables', () => {
+    beforeEach(() => {
+        cy.goHome()
+        cy.login()
+        cy.userLoggedIn() // Checkpoint (Valida que o login foi bem-sucedido)
+        cy.goTo('/tables', 'Tables')
+    })
+
+    it('Validar a linha do Github', () => {
+        cy.contains('table tbody tr', '1004')
+            .should('be.visible')
+            .and('contain', 'papitodev')
+            .and('contain', 'Ativo')
+    })
+
+    it('Remover uma rede social', () => {
+        const name = 'Facebook'
+
+        cy.contains('table tbody tr', 1002)
+            .find('.remove-item')
+            .click()
+        cy.contains('button', 'Excluir')
+            .click()
+        cy.get('table tbody')
+            .should('not.contain', name)
+    })
+
+    it('Permanecer na tabela ao desistir da exclusão de uma rede social', () => {
+        const name = 'Youtube'
+
+        cy.contains('table tbody tr', 1005)
+            .find('.remove-item')
+            .click()
+        cy.contains('button', 'Cancelar')
+            .click()
+        cy.get('table tbody')
+            .should('contain', name)
+    })
+});
