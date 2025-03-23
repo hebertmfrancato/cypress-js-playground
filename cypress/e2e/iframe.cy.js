fe251a4af1a34a38fb5013f78a2d050702a805ee
hebertmfrancato <hebertmfrancato@gmail.com>
2025-03-23 16:30:42 -0300

    Adiciona teste de Iframe

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -1,3 +1,19 @@
 describe('Iframe', () => {
-    
+    beforeEach(() => {
+        cy.goHome()
+        cy.doLogin()
+        cy.goTo('/iframe', 'IFrame')
+    })
+
+    it('Deve preencher o nome em uma página que tem Iframe', () => {
+        
+        cy.get('[data-cy="iframe-inputs"]')
+            .then(($iframe) => {
+                const $body = $iframe.contents().find('body')
+                
+                cy.wrap($body)
+                    .find('#fullname')
+                    .type('Hebert Francato')
+            })
+    })
 });
